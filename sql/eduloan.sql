fe869ef5c3b94a49faed1eee82866b0bd78bfa05
Priyanshu Kumar <priyanshukm@gmail.com>
2017-06-15 13:34:55 +0530

    Changed EduLoan

--- sql/eduloan.sql
+++ sql/eduloan.sql
@@ -12,8 +12,6 @@ CREATE TABLE IF NOT EXISTS homeloan(
        LoanLimit int,
        Security varchar(100),
        Gender varchar(100),
-       AgeLowerLimit int,
-       AgeUpperLimit int,
        Concession float,
        Comments varchar(100),
        PRIMARY KEY (Id)

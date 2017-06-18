e57d7037e832d81e665674b5bcab76629f7b1898
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-06-18 18:02:11 +0530

    add review submission feature

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from models import *
 
 app = create_app()
@@ -12,6 +12,7 @@ db.create_all(app=app)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html')
 
@@ -24,16 +25,55 @@ def customer():
 def product_info(id):
     product = Product.query.filter_by(id=id).first()
     reviews = Review.query.filter_by(productid=id).all()
+    me = User.query.filter_by(id=1).first()
     print(product)
     print(reviews)
-    return render_template('product.html', product=product, reviews=reviews)
+    return render_template('product.html',
+                           product=product,
+                           reviews=reviews,
+                           me=me)
+
+
+@app.route("/submit_review", methods=["POST"])
+def submit_review():
+    """ The request form should contain following:
+    userid, productid, rating, comment (exact name)
+    """
+    try:
+        productid = int(request.form["productid"])
+        userid = int(request.form["userid"])
+        rating = request.form["rating"]
+        comment = request.form["comment"]
+        # check existing review
+        rev = Review.query.filter_by(productid=productid, userid=userid).first()
+        print("existing review", rev)
+        if not rev:
+            new_rev = Review(userid=userid, productid=productid, rating=rating, comment=comment)
+            db.session.add(new_rev)
+            print("new review added")
+        else:
+            rev.rating = rating
+            rev.comment = comment
+            rev.created_at = datetime.now()
+            print("review updated")
+        db.session.commit()
+    except KeyError as err:
+        return "Incomplete parameters", 400
+    except sqlalchemy.exc.IntegrityError:
+        # should not happen
+        return "Database crashed", 500
+    else:
+        return "OK"
 
 
 @app.route("/admin")
 def admin():
     # report of hits on distinct products
-    logs = Log.query.filter_by()
-    return render_template("admin.html")
+    today = datetime.now().strptime("%d-%m-%Y")
+    start_time = datetime.strftime(today, "%d-%m-%Y")
+    end_time = datetime.now()
+    logs = Log.query.filter(Log.timestamp > start_time, Log.timestamp < end_time)
+    return render_template("admin.html", logs=logs)
 
 
 @app.route("/support")

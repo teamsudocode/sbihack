4c15c26ebf5f4b59753015dbb665a362c6eac170
priyanshukm <priyanshukm@gmail.com>
2017-06-18 12:58:50 +0530

    Added dummy rows

--- quant/eduloan.py
+++ quant/eduloan.py
@@ -6,7 +6,7 @@ class EduLoan(db.Model):
     EffInterestRate = db.Column(db.Float)
     ResetPeriod = db.Column(db.Float)
     Nationality = db.Column(db.String(100))
-    InstituteName = db.Column(db.String(100))
+    CourseType = db.Column(db.String(100))
     InstituteType = db.Column(db.String(100))
     InstituteCountry = db.Column(db.String(100))
     LoanLimit = db.Column(db.Integer)
@@ -15,16 +15,16 @@ class EduLoan(db.Model):
     Concession = db.Column(db.Float)
     Comments = db.Column(db.String(100))
 
-    def __init__(self, Id, LoanType, MCLR1Year, Spread, EffInterestRate, ResetPeriod, Nationality, InstituteName,
+    def __init__(self, Id, LoanType, MCLR1Year, Spread, EffInterestRate, ResetPeriod, Nationality, CourseType,
                  InstituteType, InstituteCountry, LoanLimit, Security, Gender, Concession, Comments):
         self.Id = Id
-        self.LoanLimit = LoanLimit
+        self.LoanType = LoanType
         self.MCLR1Year = MCLR1Year
         self.Spread = Spread
         self.EffInterestRate = EffInterestRate
         self.ResetPeriod = ResetPeriod
         self.Nationality = Nationality
-        self.InstituteName = InstituteName
+        self.CourseType = CourseType
         self.InstituteType = InstituteType
         self.InstituteCountry = InstituteCountry
         self.LoanLimit = LoanLimit
@@ -35,3 +35,13 @@ class EduLoan(db.Model):
 
     def __repr__(self):
         return '<Eduloan %r>' % self.Id
+
+data1 = EduLoan(1, 'ScholarSBI1', 8.5, 1.0, 9.0, 1, 'Indian', 'UnderGrad', 'IIT/IIM/NIT/IIIT', 'India', 1000000, 'Parent', 'M/F', 0.0, 'Mighty IITians')
+data2 = EduLoan(2, 'ScholarSBI2', 9.5, 1.0, 10.5, 1, 'Indian', 'UnderGrad/PostGrad', 'Any', 'India', 1000000, 'Parent', 'M/F', 0.0, 'None')
+data3 = EduLoan(3, 'ScholarSBI3', 8.0, 1.5, 9.5, 1, 'Indian/NRI', 'UnderGrad/PostGrad', 'Any', 'India/Abroad', 1000000, 'Parent', 'F', 1.0, 'Ladies first')
+data4 = EduLoan(4, 'ScholarSBI4', 7.5, 1.0, 8.5, 1, 'Indian/NRI', 'UnderGrad/PostGrad', 'Any', 'India/Abroad', 1000000, 'Parent/Collateral', 'M/F', 0.0, 'None')
+db.session.add(data1)
+db.session.add(data2)
+db.session.add(data3)
+db.session.add(data4)
+db.session.commit()

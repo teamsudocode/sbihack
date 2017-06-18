3a3d885f9d1b152812ec150f35e89792a7576ea7
Souvik Sen <souviksen44@gmail.com>
2017-06-18 21:22:35 +0530

    Added Homeloan Schema. Should we shift this to models.py

---
+++ quant/homeloan.py
@@ -0,0 +1,40 @@
+class Homeloan(db.Model):
+    Id = db.Column(db.Integer, primary_key = True)
+    LoanType = db.Column(db.String(80))
+    LoanName = db.Column(db.String(100))
+    InterestRate = db.Column(db.Float)
+    TenureLowerLimit = db.Column(db.Integer)
+    TenureUpperLimit = db.Column(db.Integer)
+    PrincipalLowerLimit = db.Column(db.Integer)
+    PrincipalUpperLimit = db.Column(db.Integer)
+    PrePaymentPenalty = db.Column(db.Integer)
+    FlexiPay = db.Column(db.Integer)
+    ageLowerLimit = db.Column(db.Integer)
+    ageUpperLimit = db.Column(db.Integer)
+    CustomerType = db.Column(db.String(100))
+    Comments = db.Column(db.String(100))
+
+
+    def __init__(self, Id, LoanType, LoanName, InterestRate, TenureLowerLimit, TenureUpperLimit, PrincipalLowerLimit, PrincipalUpperLimit, PrePaymentPenalty, FlexiPay, ageLowerLimit, ageUpperLimit, CustomerType, Comments):
+        self.Id = Id
+        self.LoanType = LoanType
+        self.LoanName = LoanName
+        self.InterestRate = InterestRate
+        self.TenureLowerLimit = TenureLowerLimit
+        self.TenureUpperLimit = TenureUpperLimit
+        self.PrincipalLowerLimit = PrincipalLowerLimit
+        self.PrincipalUpperLimit = PrincipalUpperLimit
+        self.PrePaymentPenalty = PrePaymentPenalty
+        self.FlexiPay = FlexiPay
+        self.ageLowerLimit = ageLowerLimit
+        self.ageUpperLimit = ageUpperLimit
+        self.CustomerType = CustomerType
+        self.Comments = Comments
+
+    def __repr__(self):
+        return '<Homeloan %r' % self.Id
+
+
+"""
+Will add data tonight
+"""
